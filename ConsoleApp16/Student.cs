77ecf4c8913440386ca888a04e6536cbf8c0e0ce
Алексей Лисичников <alexochanel@gmail.com>
2024-04-09 13:24:07 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp16/Student.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp16
+{
+    struct Student
+    {
+        public string Name;
+        public int GroupNumber;
+        public int[] Grades;
+
+        public Student(string Name, int GroupNumber, int[] Grades)
+        {
+            this.Name = Name;
+            this.GroupNumber = GroupNumber;
+            this.Grades = Grades;
+
+        }
+        public double AverageGrade
+        {
+            get { return Grades.Average(); }
+        }
+    }
+}
